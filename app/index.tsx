a8c2d30e17d8363997f9ba53cc60308a57478687
enocben <benenoc5@gmail.com>
2025-03-26 01:20:55 +0200

    feat(app/index.tsx): Add WebView component to render external content

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { WebView } from "react-native-webview"
+import Constant from 'expo-constants'
+import { StatusBar } from 'expo-status-bar'
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <View style={styles.container}>
+      <WebView
+        source={{ uri: "https://gemini.google.com" }}
+      />
+      <StatusBar style="dark" />
+      </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constant.statusBarHeight
+  }
+})
